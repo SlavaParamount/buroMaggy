6b4362841eb1818ff2c65401f5000aa5b8bf21bd
SlavaParamount <ViParamount@4261811f904c>
2019-08-02 07:23:30 +0000

    deleted database login data

--- app/config.py
+++
@@ -1,4 +0,0 @@
-class Configuration(object):
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:K219W*?D{i@localhost:5433/learn'
-    WTF_CSRF_ENABLED = False
